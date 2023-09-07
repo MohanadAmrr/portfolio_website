2e19bd786ec5e2ac8976191fda476b69f899ecb3
MohanadAmrr <mohanadbarakat1998@gmail.com>
2023-09-07 09:17:06 +0300

    ja

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -114,7 +114,7 @@ export const TimeLineData = [
   { year: 2020, text: "Interned as a Front-End Developer" },
   {
     year: 2021,
-    text: "Graduated from University and started freelancing as a web developer",
+    text: "Graduate from University and started freelancing as a web developer",
   },
   {
     year: 2022,
